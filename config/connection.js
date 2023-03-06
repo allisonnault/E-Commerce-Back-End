7df12dd4c78978226debe20aed05bc443af98874
allisonnault <allisonrnault@gmail.com>
2023-03-06 10:14:09 -0600

    saving

--- config/connection.js
+++ config/connection.js
@@ -7,6 +7,9 @@ const sequelize = process.env.JAWSDB_URL
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: '127.0.0.1',
       dialect: 'mysql',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
       port: 3306,
     });
 
